1d86552cdf23c70a1caffb7ca90f0700b2c33698
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-11-16 02:27:43 +0600

    added another style trsc template

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Home from "./Components/Home/Home";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Hello World</h1>
       <Home />
     </div>
