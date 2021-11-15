4100c0f70c3f7628c45f3908a5b8bf4b8af7d695
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-11-16 02:23:00 +0600

    created a components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
+import Home from "./Components/Home/Home";
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello World</h1>
+      <h1>Hello World</h1>
+      <Home />
     </div>
   );
 }
