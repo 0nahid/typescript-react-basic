09c235a6474a9443f4332be7fa69aebfa9ebfba4
Nahid Hassan Bulbul <41489906+0nahid@users.noreply.github.com>
2022-06-13 09:40:54 +0000

    push from codespace

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -5,11 +5,11 @@ interface IHeaderProps {}
 
 const Header: React.FunctionComponent<IHeaderProps> = (props) => {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/feature">Feature</Link>
-      <Link to="/contact">Contact</Link>
+    <div className="w-25 mx-auto">
+      <Link to="/" style={{marginRight:'10px'}}>Home</Link>
+      <Link to="/about" style={{marginRight:'10px'}}>About</Link>
+      <Link to="/feature" style={{marginRight:'10px'}}>Feature</Link>
+      <Link to="/contact" style={{marginRight:'10px'}}>Contact</Link>
     </div>
   );
 };
